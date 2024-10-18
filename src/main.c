0a387b53283a4698608d31350553ef6347821fc9
winston <winston.ong92@gmail.com>
2024-10-18 17:50:47 -0400

    Toggle portD pin 0

--- src/main.c
+++ src/main.c
@@ -2,12 +2,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-// Main
-int main(void) {
-    // Enable output port on LED register
+void init_port_led_reg(void){
     DDRB = DDRB | (1 << DDB5);
+}
 
-    while(1) {
+void toggle_board_led(void) {
         // Light LED
         PORTB = PORTB | (1 << PORTB5);
 
@@ -19,6 +18,32 @@ int main(void) {
 
         // WAIT
         _delay_ms(100);
+
+}
+
+void port_d_init(void){
+    DDRD = DDRD | 0x1;
+}
+
+void toggle_d0(void){
+    // Turn on D0
+    PORTD = PORTD | 0x1;
+
+    _delay_ms(1000);
+    PORTD = 0;
+
+    _delay_ms(1000);
+}
+
+// Main
+int main(void) {
+    // Enable output port on LED register
+    init_port_led_reg();
+    port_d_init();
+
+    while(1) {
+        toggle_board_led();
+        toggle_d0();
     }
 
     // End
